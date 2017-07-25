6019b613112df02157626a6c0333e3924b0c5cc4
sqrtofsaturn <sqrtofsaturn@gmail.com>
2017-07-25 13:26:15 -0700

    Maybe it'll work?

---
+++ command.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const { MeshbluConnectorCommand } = require('meshblu-connector-cli')
+
+const command = new MeshbluConnectorCommand({ argv: process.argv, connectorPath: __dirname })
+command.run()
